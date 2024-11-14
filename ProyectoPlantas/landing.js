6e8402b6b4b2af38e2028ec1d7fb8f48f2a863f0
Fifthexpert9 <150827829+Fifthexpert9@users.noreply.github.com>
2024-11-14 12:49:58 +0100

    Intentando modificar javaScript

--- ProyectoPlantas/landing.js
+++ ProyectoPlantas/landing.js
@@ -1,5 +1,5 @@
 let boton = document.getElementById('SobreNosotros');
 
 boton.addEventListener('click', () => {
-    window.open("../1er Sprint Empresa.pdf", '_blank').focus();
+    window.open("https://github.com/Fifthexpert9/TekNova-SemperVivum/blob/main/1er%20Sprint%20Empresa.pdf", '_blank').focus();
 });
