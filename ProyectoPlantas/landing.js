51667041f774e659a8b0a9580e6295cac9abe881
Fifthexpert9 <150827829+Fifthexpert9@users.noreply.github.com>
2024-11-14 13:21:46 +0100

    Añadido boton para llevar a Github

--- ProyectoPlantas/landing.js
+++ ProyectoPlantas/landing.js
@@ -1,10 +1,20 @@
 let botones = document.querySelectorAll('.empresaPDF');
 
 botones.forEach(boton => {
-    
+
     boton.addEventListener('click', () => {
 
         window.open("../1er Sprint Empresa.pdf", '_blank').focus();
     });
+});
+
+
+
+
+let hablarBtn = document.getElementById('hablemos');
+
+hablarBtn.addEventListener('click', () => {
+
+    window.open("https://github.com/Fifthexpert9", '_blank').focus();
 });
     
