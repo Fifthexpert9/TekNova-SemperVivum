6e2651de812c8e64b941003d72fe2d2d5c7c9fd5
Fifthexpert9 <150827829+Fifthexpert9@users.noreply.github.com>
2024-11-14 13:34:21 +0100

    Añadida funcionalidad en beta del Login

--- ProyectoPlantas/landing.js
+++ ProyectoPlantas/landing.js
@@ -17,4 +17,22 @@ hablarBtn.addEventListener('click', () => {
 
     window.open("https://github.com/Fifthexpert9", '_blank').focus();
 });
+
+
+
+let formularioContenedor = document.getElementById('contenedor');
+let botonLog = document.getElementById('Login');
+
+botonLog.addEventListener('click', () => {
+					
+    formularioContenedor.style.display = "flex";
+
+});
+
+
+formularioContenedor.addEventListener('click', () => {
+						
+    formularioContenedor.style.display = "none";
+
+});
     
