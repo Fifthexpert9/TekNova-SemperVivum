f84f7db48ca5100d233bf7052e28c35d2f7972fe
Javier Martínez del Barrio <javiermdeb@gmail.com>
2025-02-14 10:35:41 +0100

    Updated SQL DDL script

--- sql/v1.sql
+++ sql/v1.sql
@@ -57,10 +57,23 @@ CREATE TABLE orders (
 
     user_id INT NOT NULL,
     total_in_cents INT NOT NULL,
-    status ENUM('pending', 'completed', 'cancelled') NOT NULL DEFAULT 'pending',
 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE order_items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
 );
