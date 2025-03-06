e0b3a6a4393deadec0c5f2bf96c406a816b99339
MrSevyu <javiermdeb@gmail.com>
2025-03-06 10:47:01 +0100

    Fixed product image problem (?)

--- include/header.include.php
+++ include/header.include.php
@@ -6,31 +6,19 @@ use Core\Auth;
 <body class="d-flex flex-column align-items-center vh-100 min-vh-100">
 <header class="navbar navbar-expand-lg navbar-light bg-light fixed-top mb-4">
     <div class="container-fluid">
-        <!-- Logo -->
         <a id="logo" class="navbar-brand" href="<?= Routes::HOME ?>">
             <img src="/assets/imgs/logo/logo.png" alt="logo" class="d-inline-block align-text-top">
         </a>
 
-        <!-- Botón de menú responsive -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <!-- Menú de navegación -->
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav text-center">
                 <li class="nav-item">
                     <a class="text-uppercase" href="<?= Routes::GALLERY ?>">Plantas</a>
-                </li>
-                <li class="nav-item">
-                    <a class="text-uppercase" href="#">Flores</a>
-                </li>
-                <li class="nav-item">
-                    <a class="text-uppercase" href="#">Accesorios</a>
-                </li>
-                <li class="nav-item">
-                    <a class="text-uppercase" href="#">Mystery Plantbox</a>
                 </li>
 
                 <?php if (Auth::isLoggedIn()) { ?>
--- run.bat
+++ run.bat
@@ -1,10 +1,12 @@
 @echo off
 
 SET SERVER_URL=localhost:8080
-SET UPLOADS_FOLDER=uploads
+SET UPLOADS_FOLDER=uploads uploads\products
 
 echo "Creating necessary folders..."
-if not exist %UPLOADS_FOLDER% mkdir %UPLOADS_FOLDER%
+for %%i in (%UPLOADS_FOLDER%) do (
+    if not exist %%i mkdir %%i
+)
 
 echo "Starting the server..."
-C:\xampp\php\php.exe -S %SERVER_URL%
+php.exe -S %SERVER_URL%
